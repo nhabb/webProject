87dd4fd00508f5c710eb86c712966a9f116f7dcb
nhabb <nehmehabr19@gmail.com>
2024-12-16 19:07:41 +0200

    js and navbar addidtions

--- src/js/teams.js
+++ src/js/teams.js
@@ -1,14 +1,14 @@
 const teams = [
-    { rank: 1, name: "McLaren", points: 666, driver1: "Lando Norris",            image: "norris.png", logo: "UK.png", driver2:"Oscar Piastri",resreve:"Robert Shwartzman"},
-    { rank: 2, name: "Ferrari", points: 652, driver1: "Charles Leclerc",         image: "charles.png", logo: "Monaco.png", driver2:"Carlos Sainz"},
-    { rank: 3, name: "Red Bull Racing", points: 589, driver1: "Max Verstappen",  image: "max.png", logo: "netherlands.png" ,driver2:"Sergio Perez"},
-    { rank: 4, name: "Mercedes", points: 468, driver1: "Lewis Hamilton",         image: "russel.png", logo: "🇬🇧.png", driver2:"George Russell"},
-    { rank: 5, name: "Aston Martin", points: 94, driver1: "Fernando Alonso",     image: "alonso.png", logo: "🇪🇸.png", driver2:"Lance Stroll"},
-    { rank: 6, name: "Alpine", points: 65, driver1: "Pierre Gasly",              image: "gasly.png", logo: "🇫🇷.png", driver2:"Esteban Ocon"},
-    { rank: 7, name: "Haas", points: 58, driver1: "Nico Hülkenberg",             image: "hulkenberg.png", logo: "🇩🇪.png", driver2:"Kevin Magnussen"},
-    { rank: 8, name: "RB", points: 46, driver1: "Yuki Tsunoda",                  image: "yuki.png", logo: "🇯🇵.png", driver2:"Daniel Ricciardo",reserve:"Liam Lawson"},
-    { rank: 9, name: "Williams", points: 17, driver1: "Alexander Albon",         image: "albon.png", logo: "🇹🇭.png", driver2:"Logan Sargeant"},
-    { rank: 10, name: "Alfa Romeo", points: 4, driver1: "Valtteri Bottas",       image: "bottas.png", logo: "🇫🇮.png", driver2:"Zhou Guanyu"},
+    { rank: 1, name: "McLaren", points: 666, driver1: "Lando Norris",            image: "mc-car.png", logo: "mc-logo.png", driver2:"Oscar Piastri"},
+    { rank: 2, name: "Ferrari", points: 652, driver1: "Charles Leclerc",         image: "fer-car.png", logo: "fer-logo.png", driver2:"Carlos Sainz"},
+    { rank: 3, name: "Red Bull Racing", points: 589, driver1: "Max Verstappen",  image: "rbb-car.png", logo: "rbb-logo.png" ,driver2:"Sergio Perez"},
+    { rank: 4, name: "Mercedes", points: 468, driver1: "Lewis Hamilton",         image: "mer-car.png", logo: "mer-logo.png", driver2:"George Russell"},
+    { rank: 5, name: "Aston Martin", points: 94, driver1: "Fernando Alonso",     image: "am-car.png", logo: "am-logo.png", driver2:"Lance Stroll"},
+    { rank: 6, name: "Alpine", points: 65, driver1: "Jack Doohan",              image: "alp-car.png", logo: "alp-logo.png", driver2:"Esteban Ocon"},
+    { rank: 7, name: "Haas", points: 58, driver1: "Nico Hülkenberg",             image: "haas-car.png", logo: "hass-logo.png", driver2:"Kevin Magnussen"},
+    { rank: 8, name: "RB", points: 46, driver1: "Yuki Tsunoda",                  image: "RB-car.png", logo: "RB-logo.png", driver2:"Liam Lawson"},
+    { rank: 9, name: "Williams", points: 17, driver1: "Alexander Albon",         image: "wil-car.png", logo: "wil-logo.png", driver2:"Franco Colapinto"},
+    { rank: 10, name: "Kick Sauber", points: 4, driver1: "Valtteri Bottas",       image: "kick-car.png", logo: "kick-logo.png", driver2:"Zhou Guanyu"},
   ];      
 
   teams.forEach(team => {
