10964348c3e4c6fd5384ba2d4517b94cbf0e94d6
joemazloum10 <joemazloum10@gmail.com>
2024-12-18 12:56:28 +0200

    eemol pull

--- src/js/drivers.js
+++ src/js/drivers.js
@@ -2,29 +2,29 @@ document.addEventListener("DOMContentLoaded", () => {
     const container = document.getElementById("player-container");
   
     const players = [
-        { rank: 1, name: "Max ", lastName: "Verstappen", points: 437, team: "red-bull-racing-logo.png", image: "max.png", country: "NL.png" },
+        { rank: 1, name: "Max ", lastName: "Verstappen", points: 437, team: "red-bull-racing-logo.png", image: "verstappen.png", country: "NL.png" },
         { rank: 2, name: "Lando ", lastName: "Norris", points: 374, team: "mc-logo.png", image: "norris.png", country: "UK.png" },
-        { rank: 3, name: "Charles ", lastName: "Leclerc", points: 356, team: "fer-logo.png", image: "charles.png", country: "MC.png" },
-        { rank: 4, name: "Oscar ", lastName: "Piastri", points: 292, team: "mc-logo.png", image: "piastry.png", country: "au.png" },
-        { rank: 5, name: "Carlos ", lastName: "Sainz", points: 290, team: "fer-logo.png", image: "carlos.png", country: "es.png" },
-        { rank: 6, name: "George ", lastName: "Russell", points: 245, team: "mer-logo.png", image: "russel.png", country: "uk.png" },
+        { rank: 3, name: "Charles ", lastName: "Leclerc", points: 356, team: "fer-logo.png", image: "leclerc.png", country: "MC.png" },
+        { rank: 4, name: "Oscar ", lastName: "Piastri", points: 292, team: "mc-logo.png", image: "piastri.png", country: "au.png" },
+        { rank: 5, name: "Carlos ", lastName: "Sainz", points: 290, team: "fer-logo.png", image: "sainz.png", country: "es.png" },
+        { rank: 6, name: "George ", lastName: "Russell", points: 245, team: "mer-logo.png", image: "russell.png", country: "uk.png" },
         { rank: 7, name: "Lewis ", lastName: "Hamilton", points: 223, team: "mer-logo.png", image: "hamilton.png", country: "uk.png" },
         { rank: 8, name: "Sergio ", lastName: "Perez", points: 152, team: "red-bull-racing-logo.png", image: "perez.png", country: "mx.png" },
         { rank: 9, name: "Fernando ", lastName: "Alonso", points: 70, team: "am-logo.png", image: "alonso.png", country: "es.png" },
         { rank: 10, name: "Pierre ", lastName: "Gasly", points: 42, team: "alp-logo.png", image: "gasly.png", country: "fr.png" },
         { rank: 11, name: "Nico ", lastName: "Hülkenberg", points: 41, team: "haas-logo.png", image: "hulkenberg.png", country: "dk.png" },
-        { rank: 12, name: "Yuki ", lastName: "Tsunoda", points: 30, team: "rbb-logo.png", image: "yuki.png", country: "jp.png" },
+        { rank: 12, name: "Yuki ", lastName: "Tsunoda", points: 30, team: "rbb-logo.png", image: "tsunoda.png", country: "jp.png" },
         { rank: 13, name: "Lance ", lastName: "Stroll", points: 24, team: "am-logo.png", image: "stroll.png", country: "Cn.png" },
-        { rank: 14, name: "Esteban ", lastName: "Ocon", points: 23, team: "alp-logo.png", image: "doohan.png", country: "fr.png" },
-        { rank: 15, name: "Kevin ", lastName: "Magnussen", points: 16, team: "haas-logo.png", image: "magnusen.png", country: "dk.png" },
+        { rank: 14, name: "Esteban ", lastName: "Ocon", points: 23, team: "alp-logo.png", image: "ocon.png", country: "fr.png" },
+        { rank: 15, name: "Kevin ", lastName: "Magnussen", points: 16, team: "haas-logo.png", image: "magnussen.png", country: "dk.png" },
         { rank: 16, name: "Alexander ", lastName: "Albon", points: 12, team: "wil-logo.png", image: "albon.png", country: "th.png" },
-        { rank: 17, name: "Daniel ", lastName: "Ricciardo", points: 12, team: "rbb-logo.png", image: "riccardo.png", country: "au.png" },
+        { rank: 17, name: "Daniel ", lastName: "Ricciardo", points: 12, team: "rbb-logo.png", image: "ricciardo.png", country: "au.png" },
         { rank: 18, name: "Oliver ", lastName: "Bearman", points: 7, team: "Haas-logo.png", image: "bearman.png", country: "UK.png" },
         { rank: 19, name: "Franco ", lastName: "Colapinto", points: 5, team: "wil-logo.png", image: "colapinto.png", country: "arg.png" },
-        { rank: 20, name: "Zhou ", lastName: "Guanyu", points: 4, team: "kick-logo.png", image: "zhou.png", country: "Ch.png" },
+        { rank: 20, name: "Zhou ", lastName: "Guanyu", points: 4, team: "kick-logo.png", image: "guanyu.png", country: "Ch.png" },
         { rank: 21, name: "Liam ", lastName: "Lawson", points: 4, team: "rbb-logo.png", image: "lawson.png", country: "NZ.png" },
         { rank: 22, name: "Valtteri ", lastName: "Bottas", points: 0, team: "kick-logo.png", image: "bottas.png", country: "fi.png" },
-        { rank: 23, name: "Logan ", lastName: "Sargeant", points: 0, team: "wil-logo.png", image: "seargant.png", country: "us.png" },
+        { rank: 23, name: "Logan ", lastName: "Sargeant", points: 0, team: "wil-logo.png", image: "sargeant.png", country: "us.png" },
         { rank: 24, name: "Jack ", lastName: "Doohan", points: 0, team: "alp-logo.png", image: "doohan.png", country: "AU.png" },
       ];      
   
@@ -43,21 +43,21 @@ document.addEventListener("DOMContentLoaded", () => {
             <div class="separator-line"></div>
             <div class="player-name">
             <div class="player-dets">
-                <div class="name-title">${player.name}</div>
-                <div class="name">${player.lastName}</div>
+                <span class="name-title">${player.name}</span><br>
+                <span class="name">${player.lastName}</span>
                 </div>
              <div class="flag">
                 <img src="../Images/${player.country}" alt="${player.country}">
-            </div>
-            </div>
+              </div>
+              </div>
             <div class="separator-line"></div>
             <div class="images">
             <div class="team-image">
             <img src="../Images/${player.team}">
-            </div>
+                </div>
             <div class="player-image">
             <img src="../Images/${player.image}" alt="${player.name} Image">
-            </div>
+                </div>
             </div>
         </div>
       `;
--- src/js/teams.js
+++ src/js/teams.js
@@ -1,50 +1,64 @@
-const teams = [
-    { rank: 1, name: "McLaren", points: 666, driver1: "Lando Norris",            image: "mc-car.png", logo: "mc-logo.png", driver2:"Oscar Piastri"},
-    { rank: 2, name: "Ferrari", points: 652, driver1: "Charles Leclerc",         image: "fer-car.png", logo: "fer-logo.png", driver2:"Carlos Sainz"},
-    { rank: 3, name: "Red Bull Racing", points: 589, driver1: "Max Verstappen",  image: "rbb-car.png", logo: "rbb-logo.png" ,driver2:"Sergio Perez"},
-    { rank: 4, name: "Mercedes", points: 468, driver1: "Lewis Hamilton",         image: "mer-car.png", logo: "mer-logo.png", driver2:"George Russell"},
-    { rank: 5, name: "Aston Martin", points: 94, driver1: "Fernando Alonso",     image: "am-car.png", logo: "am-logo.png", driver2:"Lance Stroll"},
-    { rank: 6, name: "Alpine", points: 65, driver1: "Jack Doohan",              image: "alp-car.png", logo: "alp-logo.png", driver2:"Esteban Ocon"},
-    { rank: 7, name: "Haas", points: 58, driver1: "Nico Hülkenberg",             image: "haas-car.png", logo: "hass-logo.png", driver2:"Kevin Magnussen"},
-    { rank: 8, name: "RB", points: 46, driver1: "Yuki Tsunoda",                  image: "RB-car.png", logo: "RB-logo.png", driver2:"Liam Lawson"},
-    { rank: 9, name: "Williams", points: 17, driver1: "Alexander Albon",         image: "wil-car.png", logo: "wil-logo.png", driver2:"Franco Colapinto"},
-    { rank: 10, name: "Kick Sauber", points: 4, driver1: "Valtteri Bottas",       image: "kick-car.png", logo: "kick-logo.png", driver2:"Zhou Guanyu"},
-  ];      
+document.addEventListener("DOMContentLoaded", () => {
+    const container = document.getElementById("team-container");
 
-  teams.forEach(team => {
-    const box = document.createElement("div");
-    box.className = "team-box";
-    box.innerHTML = `
+    const teams = [
+        { rank: 1, name: "McLaren", points: 666, driver1: "Norris", driver1First:"Lando", image: "mc-car.png", logo: "mc-logo.png", driver2: "Piastri", driver2First:"Oscar", },
+        { rank: 2, name: "Ferrari", points: 652, driver1: "Leclerc", driver1First:"Charles", image: "fer-car.png", logo: "fer-logo.png", driver2: "Sainz", driver2First:"Carlos" },
+        { rank: 3, name: "Red Bull Racing", points: 589, driver1: "Verstappen", driver1First:"Max", image: "rbb-car.png", logo: "red-bull-racing-logo.png", driver2: "Perez", driver2First:"Sergio" },
+        { rank: 4, name: "Mercedes", points: 468, driver1: "Hamilton", driver1First:"Lewis", image: "mer-car.png", logo: "mer-logo.png", driver2: "Russell", driver2First:"George" },
+        { rank: 5, name: "Aston Martin", points: 94, driver1: "Alonso", driver1First:"Fernando", image: "am-car.png", logo: "am-logo.png", driver2: "Stroll", driver2First:"Lance" },
+        { rank: 6, name: "Alpine", points: 65, driver1: "Doohan", driver1First:"Jack", image: "alp-car.png", logo: "alp-logo.png", driver2: "Ocon", driver2First:"Esteban" },
+        { rank: 7, name: "Haas", points: 58, driver1: "Hulkenberg", driver1First:"Nico", image: "haas-car.png", logo: "haas-logo.png", driver2: "Magnussen", driver2First:"Kevin" },
+        { rank: 8, name: "RB", points: 46, driver1: "Tsunoda", driver1First:"Yuki", image: "RB-car.png", logo: "Rbb-logo.png", driver2: "Lawson", driver2First:"Liam" },
+        { rank: 9, name: "Williams", points: 17, driver1: "Albon", driver1First:"Alexander", image: "wil-car.png", logo: "wil-logo.png", driver2: "Colapinto", driver2First:"Franco" },
+        { rank: 10, name: "Kick Sauber", points: 4, driver1: "Bottas", driver1First:"Valtteri", image: "kick-car.png", logo: "kick-logo.png", driver2: "Guanyu", driver2First:"Zhou" },
+    ];
+
+    teams.forEach(team => {
+        const box = document.createElement("div");
+        box.className = "team-box";
+        box.innerHTML = `
     <div class="first-div">
         <div class="rank">${team.rank}</div>
-        <div class="pts">${team.points}PTS</div>
-    </div>
+        <div>
+        <span class="pts">${team.points}</span>
+        <span>PTS</span>
+        </div>
+            </div>
     <div class="separator-line"></div>
-    <div class="">
+    <div class="second-div">
         <div class="team-name">${team.name}</div>
         <div class="team-logo">
-        <img src="${team.logo}" alt="${team.name} logo">
+        <img src="../images/${team.logo}" alt="${team.name} logo">
         </div>
-    </div>
+            </div>
     <div class="separator-line"></div>
-    <div class="players-names">
+    <div class="third-div">
         <div class="driver1">
-        <div class="driver1-name">${team.driver1}</div>
+        <div class="driver1-name">
+        <span class="first1">${team.driver1First}</span><br>
+        <span class="last1">${team.driver1}</span>
+        </div>
         <div class="driver1-img">
-        <img src="../images/${team.driver1}.png">
-        </div>
-        </div>
+        <img src="../images/${team.driver1}.png" alt="${team.driver1} image"></div>
+            </div>
         <div class="driver2">
-        <div class="driver2-name">${team.driver2}</div>
+        <div class="driver2-name">
+        <span class="first2">${team.driver2First}</span><br>
+        <span class="last2">${team.driver2}</span></div>
         <div class="driver2-img">
-        <img src="../images/${team.driver2}.png">
-        </div>
-        </div>
+        <img src="../images/${team.driver2}.png" alt="${team.driver2} image"></div>
+            </div>
     </div>
+    <div class="fourth-div">
+    <img src="../images/${team.image}" alt="${team.image}"></div>
+        </div>
+    </div>    
 
     
 
+    `;
+        container.appendChild(box);
+    });
+});
 
-    `
-    
-  });
