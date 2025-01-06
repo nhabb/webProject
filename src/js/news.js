eb33876f69aaa8c01fd2dd42465bd5efb36659be
nhabb <nehmehabr19@gmail.com>
2025-01-06 19:35:42 +0200

    several changes in news page

--- src/js/news.js
+++ src/js/news.js
@@ -1,15 +1,14 @@
 window.onload = () => {
-    const photo = document.getElementById("photo");
-    const video = document.getElementById("video");
-  
-    // Show the photo first
-    photo.classList.remove("hidden");
-  
-    // Wait for 5 seconds, then show the video
-    setTimeout(() => {
+  const photo = document.getElementById("photo");
+  const video = document.getElementById("video");
+
+  // Show the photo first
+  photo.classList.remove("hidden");
+
+  // Wait for 5 seconds, then show the video
+  setTimeout(() => {
       photo.classList.add("hidden"); // Hide the photo
       video.classList.remove("hidden"); // Show the video
       video.play(); // Ensure the video starts playing
-    }, 5000); // 5000ms = 5 seconds
-  };
-  
+  }, 5000); // 5000ms = 5 seconds
+};
